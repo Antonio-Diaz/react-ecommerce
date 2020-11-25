c3f4df00f02e233ffa0308804911f511f94ea6b4
Antonio Diaz <j.antonio.udiaz@gmail.com>
2020-11-25 11:04:32 -0600

    implemented firebase utils in the app, this included to store for the authentication with email and password, google accounts

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -32,7 +32,7 @@ class SignIn extends React.Component {
     render(){
         return(
             <div className="sign-in">
-                <h2>I have already account</h2>
+                <h2>I already have an account</h2>
                 <span>sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>                    
                     <FormInput onChange={this.handleChange}
